6b1137b615b0f2adba94319767f1d6d2dfaa287a
Jakub Skokan <jakub.skokan@havefun.cz>
2014-07-15 10:29:18 +0200

    Remove deprecated utils

--- utils/stikked_lang_generator.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BASEPATH', true);
-$config = array();
-include("geshi_languages.php");
-
-foreach($config['geshi_languages'] as $key => $val) {
-	echo "{\"".$key."\", \"".$val."\"},\n";
-}
